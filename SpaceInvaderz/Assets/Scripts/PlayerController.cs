44abcbe18ad1d8711900c9b3988185a386969d78
PSDIUM <rromanomusic@gmail.com>
2019-05-16 10:15:29 +1000

    Player Movement
    Implemented acceleration and velocity for movement

--- SpaceInvaderz/Assets/Scripts/PlayerController.cs
+++ SpaceInvaderz/Assets/Scripts/PlayerController.cs
@@ -6,11 +6,13 @@ public class PlayerController : MonoBehaviour {
 
 	[Header("Physics Properties")]
 	[SerializeField] private float speed = 5;
-	[SerializeField] private float acceleration = 1;
+    [SerializeField] private float maxAcceleration = 5;
+    [SerializeField] private float accelerationStepping = 1;
+    [SerializeField] private float accelerationBase= 1;
+    private float acceleration = 1;
 
 	private void Update() {
 		Movement();
-
 	}
 
 	private void Movement() {
@@ -18,18 +20,16 @@ public class PlayerController : MonoBehaviour {
 		float inputY = Input.GetAxis("Vertical");
 		Vector2 velocity = new Vector2(inputX, inputY);
 
-		velocity *= speed * Time.deltaTime;
+		velocity *= speed * acceleration * Time.deltaTime;
 
 		transform.position += new Vector3(velocity.x, velocity.y, 0);
 
-		if (inputX>0 || inputY>0) {
-			acceleration += 1 *Time.deltaTime;
+		if (inputX!=0 || inputY!=0) {
+			acceleration += accelerationStepping;
+            if (acceleration >= maxAcceleration)
+                acceleration = maxAcceleration; 
 		} else {
-			acceleration = 1;
+			acceleration = accelerationBase;
 		}
-	}
-
-	private void Acceleration() {
-
 	}
 }
